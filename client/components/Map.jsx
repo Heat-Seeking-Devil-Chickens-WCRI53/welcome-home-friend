264efa8c834921b0f22ae2c279765797fe4c8a54
brianhip <brian7terra@gmail.com>
2022-10-06 19:19:17 -0600

    map added

--- client/components/Map.jsx
+++ client/components/Map.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import { GoogleMap, LoadScript, Marker, InfoWindow } from '@react-google-maps/api';
 
+const mapStyle = {
+    height: '100vh',
+    width: '100%',
+};
 
 const center = {
     lat: 34.052,
     lng: -118.244
-  };
+};
